22679e72261e069eac966e2dcb7d998fdef5d1c8
Dablikar Durgaprasad <prasaddablikar6@gmail.com>
2025-04-07 19:51:41 +0530

    index.js committed

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Server running!');
+});
+
+server.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
+module.exports = server;
+
