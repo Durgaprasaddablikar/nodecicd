2500cee42dd92a59bef0c8e5e19988927f7961f6
Durgaprasaddablikar <prasaddab833@gmail.com>
2025-04-07 21:09:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello, World!');
 });
 
-app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
 
-module.exports = app; // <-- this line is critical
+module.exports = server; // <--- this line is important!
